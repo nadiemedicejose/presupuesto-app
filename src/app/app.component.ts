f7378f1a8998f168a29b357502dea68f1cfd76e1
Jose Estrada <j.estrada49@icloud.com>
2021-07-10 11:49:50 -0700

    Definir arreglos y métodos sobre Ingresos/Egresos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Egreso } from './models/egreso.model';
+import { Ingreso } from './models/ingreso.model';
+import { EgresoServicio } from './services/egreso.service';
+import { IngresoServicio } from './services/ingreso.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'presupuesto-app';
+
+  // Declarar arreglos para Ingresos y Egresos
+  ingresos: Ingreso[] = []
+  egresos: Egreso[] = []
+
+  // Crear el constructor
+  constructor(
+    private ingresoServicio: IngresoServicio,
+    private egresoServicio: EgresoServicio
+  ) {
+    // Inicializar variables
+    this.ingresos = ingresoServicio.ingreso
+    this.egresos = egresoServicio.egreso
+  }
+
+  // Definir método para sumar Ingresos
+  getIngresoTotal() {
+    let ingresoTotal: number = 0
+
+    this.ingresos.forEach(ingreso => {
+      ingresoTotal += ingreso.valor
+    })
+
+    return ingresoTotal
+  }
+
+  // Definir método para sumar Egresos
+  getEgresoTotal() {
+    let egresoTotal: number = 0
+
+    this.egresos.forEach(egreso => {
+      egresoTotal += egreso.valor
+    })
+
+    return egresoTotal
+  }
+
+  // Definir método para obtener porcentaje de Ingresos vs Egresos
+  getPorcentajeTotal() {
+    return this.getEgresoTotal() / this.getIngresoTotal()
+  }
+
+  // Definir método para obtener diferencia entre Ingresos y Egresos
+  getPresupuestoTotal() {
+    return this.getIngresoTotal() - this.getEgresoTotal()
+  }
 }
